eddc7ef7ae0eac3c86bfca6ac15d783bf375f00b
SeldomseenSchweig <jamesschweig@gmail.com>
2022-06-10 06:43:42 -0700

    first commit

---
+++ calculator/calculator-test.js
@@ -0,0 +1,23 @@
+
+it('should calculate the monthly rate correctly', function () {
+  expect(calculateMonthlyPayment({amount:10000, rate: .05, years:10})).toEqual("106.07");
+  
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  expect(calculateMonthlyPayment({amount:10000, rate: .05, years:10})).toBeCloseTo("106.07",2);
+});
+
+it('should calculate the monthly rate correctly', function () {
+  expect(calculateMonthlyPayment({amount:10000, rate: .05, years:10})).toBeInstanceOf(String);
+  
+});
+
+
+// it("should return a result with 2 decimal places", function() {
+//   expect(calculateMonthlyPayment({amount:10000, rate: .05, years:10})).toBeCloseTo("106.07",2);
+// });
+
+// /// etc
+
